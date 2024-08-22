b7a6befc347cff09259349a1b9022fa3a6ea2ae4
tsai-yu-xuan <163313739+tsai-yu-xuan@users.noreply.github.com>
2024-08-22 15:28:57 +0000

    Deploying to gh-pages from @ tsai-yu-xuan/vuetify-shop-front@48dd33a3ea7939d5e52292c3462461fac150194e 🚀

---
+++ assets/VContainer-BnVQQD_B.js
@@ -0,0 +1,1 @@
+import{p as n,m as o,f as r,g as l,y as i,h as c,b as m}from"./index-BBEz_kA-.js";const u=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),f=l()({name:"VContainer",props:u(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>m(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{f as V};
